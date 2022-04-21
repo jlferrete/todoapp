f081a887898eb9f2f2f53d5704a71c11f519f9ee
jlferrete <joseluisferrete@gmail.com>
2022-04-21 20:58:39 +0200

    Added findAllByStatus()

--- src/main/java/com/sacavix/todoapp/persistence/repository/TaskRepository.java
+++ src/main/java/com/sacavix/todoapp/persistence/repository/TaskRepository.java
@@ -1,7 +1,12 @@
 package com.sacavix.todoapp.persistence.repository;
 
 import com.sacavix.todoapp.persistence.entity.Task;
+import com.sacavix.todoapp.persistence.entity.TaskStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    public List<Task> findAllByTaskStatus(TaskStatus status);
 }
