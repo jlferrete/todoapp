f1ab8f0364563454b8877c70817962fad839b803
jlferrete <joseluisferrete@gmail.com>
2022-04-21 20:41:04 +0200

    Updated Controller with createTask

--- src/main/java/com/sacavix/todoapp/controller/TaskController.java
+++ src/main/java/com/sacavix/todoapp/controller/TaskController.java
@@ -1,7 +1,26 @@
 package com.sacavix.todoapp.controller;
 
+import com.sacavix.todoapp.persistence.entity.Task;
+import com.sacavix.todoapp.service.TaskService;
+import com.sacavix.todoapp.service.dto.TaskInDTO;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/tasks")
 public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody TaskInDTO taskinDTO){
+        return this.taskService.createTask(taskinDTO);
+    }
+
 }
